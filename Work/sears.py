f48136574623829e27fbdafdff68d6d00f18c6d0
Simon <simon.c.yacob@gmail.com>
2022-01-14 17:25:04 -0500

    Completed exercise 1.6 sears.py debugging

--- Work/sears.py
+++ Work/sears.py
@@ -1,11 +1,9 @@
-#sears.py
-from tkinter import N
+# sears.py
 
-
-bill_thickness = 0.11 * 0.001 # meters (0.11mm)
-sears_height = 442 #Height (meters)
-num_bills = 1
-day = 1
+bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
+sears_height   = 442             # Height (meters)
+num_bills      = 1
+day            = 1
 
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
