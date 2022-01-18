44b2ec9c45e3d9494742ec05cb0e1656c9912840
Simon <simon.c.yacob@gmail.com>
2022-01-18 11:58:21 -0500

    Completed exercise 1.30, 1.31, 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,7 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 '''
 The columns in portfolio.csv correspond to the stock name,
  number of shares, and purchase price of a single stock holding. 
@@ -8,11 +9,15 @@ The columns in portfolio.csv correspond to the stock name,
  reads all lines, and calculates how much it cost
  to purchase all of the shares in the portfolio.
 '''
-total_cost = 0
-with open('Work/Data/portfolio.csv', 'rt') as f:
-    next(f)
-    for line in f:
-        row = line.split(',')
-        total_cost = total_cost + int(row[1]) * float(row[2])
-    
-    print('Total cost', total_cost)
+#'Work/Data/portfolio.csv'
+def portfolio_cost(filename):
+    total_cost = 0
+    try:
+        f = open(filename)
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            total_cost = total_cost + int(row[1]) * float(row[2])
+    except ValueError:
+        print('Could\'nt parse line', row)
+    return total_cost
